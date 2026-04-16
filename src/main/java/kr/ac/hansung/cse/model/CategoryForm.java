9b0cf6ae16129de76f20786a42393e803fd79d5a
gwanjung <kimkanjong@gmail.com>
2026-04-16 15:54:47 +0900

    add CategoryForm

---
+++ src/main/java/kr/ac/hansung/cse/model/CategoryForm.java
@@ -0,0 +1,9 @@
+package kr.ac.hansung.cse.model;
+
+// ProductForm.java 패턴을 참고하여 작성 (Lombok + Bean Validation)
+@Getter @Setter
+public class CategoryForm {
+    @NotBlank(message = "카테고리 이름을 입력하세요")
+    @Size(max = 50, message = "50자 이내로 입력하세요")
+    private String name; }
+
